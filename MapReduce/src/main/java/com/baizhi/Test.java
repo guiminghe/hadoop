c914271724c57f1d26ed8ad587a117833e51a0ce
guiminghe <45284735+guiminghe@users.noreply.github.com>
2018-12-19 19:14:41 +0800

    Update Test.java

--- MapReduce/src/main/java/com/baizhi/Test.java
+++ MapReduce/src/main/java/com/baizhi/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test222222");
-        System.out.println("test3333333333333");
+        System.out.println("更新更新更新");
     }
 }

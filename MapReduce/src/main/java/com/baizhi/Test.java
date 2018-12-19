ac99f0473585be62fb3160d13b20244429931227
guiminghe <865318553@qq.com>
2018-12-19 16:11:51 +0800

    3

--- MapReduce/src/main/java/com/baizhi/Test.java
+++ MapReduce/src/main/java/com/baizhi/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test222222");
+        System.out.println("test3333333333333");
     }
 }
